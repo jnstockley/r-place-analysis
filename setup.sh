78248ffead75b06c320d9bf7b5f0f7f298245488
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2023-04-26 22:20:12 -0500

    updated setup.sh script for downloading main file

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-mkdir data/
+mkdir data2/
 
-curl https://placedata.reddit.com/data/canvas-history/2022_place_canvas_history.csv.gzip -o data/data.csv.gz
+echo "Downloading file..."
 
-gzip -d data/data.csv.gz
+curl https://placedata.reddit.com/data/canvas-history/2022_place_canvas_history.csv.gzip -o data2/data.csv.gz
+
+echo "Decompressing file..."
+
+gzip -d data2/data.csv.gz
 
 local_md5sum="446315c53d6d4d422f89ef0e73b41375"
 
 echo "Checking checksum of downloaded file..."
 
-downloaded_md5sum=$(md5sum data/data.csv | awk '{ print $1 }')
+downloaded_md5sum=$(md5sum data2/data.csv | awk '{ print $1 }')
 
 if [ "$local_md5sum" = "$downloaded_md5sum" ]; then
   echo "The checksums are correct"
